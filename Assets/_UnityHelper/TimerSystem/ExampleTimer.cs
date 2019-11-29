89d796c8bde6a25215cbbf8d10af1f9311099d8f
rakibj56 <rakibj56@gmail.com>
2019-11-29 19:49:09 +0600

    Timer System added

---
+++ Assets/_UnityHelper/TimerSystem/ExampleTimer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RakibUtils
+{
+    public class ExampleTimer : MonoBehaviour
+    {
+        [SerializeField] private float timerDuration;
+        [SerializeField] private Text timerText;
+        private Timer m_timer;
+        private void Start()
+        {
+            m_timer = new Timer(timerDuration);
+        }
+
+        private void Update()
+        {
+            m_timer.ReduceTimer_Update(Time.deltaTime);
+            timerText.text = m_timer.CurrentTime.ToString("F1");
+            if(m_timer.CurrentTime <= 0)
+                m_timer.ResetTimer(timerDuration);
+        }
+    }
+}
---
+++ Assets/_UnityHelper/TimerSystem/Timer.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace RakibUtils
+{
+    public class Timer
+    {
+        private float m_currentTime = 0;
+        /// <summary>
+        /// current calculated time in timer
+        /// </summary>
+        public float CurrentTime => m_currentTime;
+
+        private float m_totalTime = 0;
+        /// <summary>
+        /// total time the timer will run for
+        /// </summary>
+        public float TotalTime => m_totalTime;
+        
+        /// <summary>
+        /// Instantiate Timer Class using the totalTime timer will run
+        /// </summary>
+        /// <param name="totalTime"></param>
+        public Timer(float totalTime)
+        {
+            ResetTimer(totalTime);
+        }
+
+        /// <summary>
+        /// Call this on Update to Run Timer
+        /// </summary>
+        /// <param name="deltaTime"></param>
+        public void ReduceTimer_Update(float deltaTime)
+        {
+            m_currentTime -= deltaTime;
+            if (m_currentTime <= 0)
+                m_currentTime = 0;
+        }
+
+        /// <summary>
+        /// Resets timer to parameter value
+        /// </summary>
+        /// <param name="totalTime"></param>
+        public void ResetTimer(float totalTime)
+        {
+            m_totalTime = totalTime;
+            m_currentTime = m_totalTime;
+        }
+    }
+}
