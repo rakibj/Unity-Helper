31fcf9eaf63aa5ab4ee58f74d831aa7b7433a475
rakibj56 <rakibj56@gmail.com>
2020-01-04 16:43:04 +0600

    PlayerPrefsExtended update

---
+++ Assets/_UnityHelper/CustomVariables/BoolValue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "RakibUtils/Boolean")]
+public class BoolValue : ScriptableObject
+{
+    public bool value;
+}
--- Assets/_UnityHelper/SimplePlayerPrefExtension/PlayerPrefsExtended.cs
+++ Assets/_UnityHelper/SimplePlayerPrefExtension/PlayerPrefsExtended.cs
@@ -4,14 +4,14 @@ namespace RakibUtils
 {
     public class PlayerPrefsExtended: PlayerPrefs
     {
-        public static int LoadOrCreateKeyInt(string key)
+        public static int LoadOrCreateKeyInt(string key, int defaultReturn)
         {
             if (PlayerPrefs.HasKey(key))
                 return PlayerPrefs.GetInt(key);
             else
             {
-                PlayerPrefs.SetInt(key, 0);
-                return 0;
+                PlayerPrefs.SetInt(key, defaultReturn);
+                return defaultReturn;
             }
         }
         public static float LoadOrCreateKeyFloat(string key)
