fdf90066905898e52909cb2ebc526e52ca7c05b5
Rakib Jahan <33758558+rakibj@users.noreply.github.com>
2021-03-01 22:55:04 +0600

    swerve input system

---
+++ Assets/_UnityHelper/InputSystems/SwerveInputSystem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwerveInputSystem : MonoBehaviour
+{
+    private float _lastFrameFingerPositionX;
+    private float _moveFactorX;
+    public float MoveFactorX => _moveFactorX;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _lastFrameFingerPositionX = Input.mousePosition.x;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            _moveFactorX = Input.mousePosition.x - _lastFrameFingerPositionX;
+            _lastFrameFingerPositionX = Input.mousePosition.x;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            _moveFactorX = 0f;
+        }
+    }
+}
---
+++ Assets/_UnityHelper/InputSystems/SwerveMovement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwerveMovement : MonoBehaviour
+{
+    private SwerveInputSystem _swerveInputSystem;
+    [SerializeField] private float swerveSpeed = 0.5f;
+    [SerializeField] private float maxSwerveAmount = 1f;
+    
+    private void Awake()
+    {
+        _swerveInputSystem = GetComponent<SwerveInputSystem>();
+    }
+
+    private void Update()
+    {
+        float swerveAmount = Time.deltaTime * swerveSpeed * _swerveInputSystem.MoveFactorX;
+        swerveAmount = Mathf.Clamp(swerveAmount, -maxSwerveAmount, maxSwerveAmount);
+        transform.Translate(swerveAmount, 0, 0);
+    }
+}
